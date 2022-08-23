afa0c3fab92de4a6323330e337c1f80a83b51770
Etizaz Ahsan <etizaz7ahsan@gmail.com>
2022-08-24 01:17:18 +0500

    continuos integration added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/ping', (req, res) => res.send('ok'))
+app.use('/ping', (req, res) => res.send('okkk'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
